f02106ff4264e910cf057bbb1f6e9e3280f753b3
Fady Besada <fbesada@amazon.com>
2020-07-23 17:31:33 -0400

    Fixed import statements

--- lambda_function.py
+++ lambda_function.py
@@ -5,14 +5,14 @@
 # session persistence, api calls, and more.
 # This sample is built using the handler classes approach in skill builder.
 import logging
-import ask_sdk_core.utils as ask_utils
+import env.ask_sdk_core.utils as ask_utils
 
-from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
-from ask_sdk_core.handler_input import HandlerInput
+from env.ask_sdk_core.skill_builder import SkillBuilder
+from env.ask_sdk_core.dispatch_components import AbstractRequestHandler
+from env.ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from env.ask_sdk_core.handler_input import HandlerInput
 
-from ask_sdk_model import Response
+from env.ask_sdk_model import Response
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
